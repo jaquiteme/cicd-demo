10e7416c11a5c70d1783cbd433651f32843e7ce8
Jordy Aquiteme <49443617+jaquiteme@users.noreply.github.com>
2023-03-24 16:07:27 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+
+APP_NAME="todo-app-front"
+RELEASE_NAME="${APP_NAME}::${GITHUB_SHA::6}"
 #DOCKER HUB LOGIN
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
-IMAGE_NAME="$DOCKER_USERNAME/motd-api:${TRAVIS_COMMIT::6}"
+IMAGE_NAME="$DOCKER_USERNAME/$RELEASE_NAME"
 #IMAGE BUILDING
 docker build -t ${IMAGE_NAME} .
 #IMAGE PUSHING
 docker push ${IMAGE_NAME}
-# GCP KUBERNETES
-kubectl set image deployment/motd-api motd-api=${IMAGE_NAME} --record --namespace jordy-aquiteme
