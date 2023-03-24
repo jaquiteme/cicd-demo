cf2a121e06161d46681c45ceb2ebc06f55fbfcbc
Jordy Aquiteme <49443617+jaquiteme@users.noreply.github.com>
2023-03-24 16:12:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 APP_NAME="todo-app-front"
-RELEASE_NAME="${APP_NAME}::${GITHUB_SHA::6}"
+RELEASE_NAME="${APP_NAME}:${GITHUB_SHA::6}"
 #DOCKER HUB LOGIN
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 IMAGE_NAME="$DOCKER_USERNAME/$RELEASE_NAME"
